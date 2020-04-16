409851e3df5cd015b5cde6bfcda6efca533d5766
ASW-研修４ <training04@ASW-kenshuu4noMacBook-Air.local>
2020-04-16 10:58:31 +0900

    first commit

---
+++ Gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gittest
+//
+//  Created by ASW-研修４ on 2020/04/16.
+//  Copyright © 2020 ASW-研修４. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
